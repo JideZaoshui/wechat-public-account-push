617a49853adb1c64b83c9221e88048fac04a5264
JideZaoshui <112365467+JideZaoshui@users.noreply.github.com>
2022-08-29 13:23:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -28,6 +28,8 @@ export const config = {
     // 生日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
     BIRTHDAYS: [
       {"name": "林小爱", "year": "2001", "date": "09-21"},
+    ],
+    BIRTHDAYS: [
       {"name": "鹏小🐟", "year": "2001", "date": "12-19"},
     ],
 
@@ -53,6 +55,7 @@ export const config = {
 //🌘最低气温: {{min_temperature.DATA}}  
 //🌞最高气温: {{max_temperature.DATA}}  
 //💕今天是我们恋爱的第{{love_day.DATA}}天
+//🎂{{birthday_message.DATA}}
 //🎂{{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
